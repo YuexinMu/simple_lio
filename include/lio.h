47f73006a0286f914507ca48fc091eb4593d7aa4
YuexinMu <2444780821@qq.com>
2025-03-20 11:20:01 +0800

    Remove some useless variables.

--- include/lio.h
+++ include/lio.h
@@ -103,10 +103,6 @@ public:
   void ObsModel(state_ikfom &state_ikf, esekfom::dyn_share_datastruct<double> &ekfom_data);
 
   // debug save / show
-//  void PublishPath(const ros::Publisher& pub_path);
-//  void PublishOdometry(const ros::Publisher &pub_odom_aft_mapped);
-//  void PublishFrameWorld();
-//  void PublishFrameBody();
   void PublishFrameEffectWorld(const ros::Publisher &pub_laser_cloud_effect_world);
   void SaveTrajectory(const std::string &traj_file);
 
@@ -154,18 +150,6 @@ private:
   std::shared_ptr<PointCloudPreprocess> preprocess_ = nullptr;  // point cloud preprocess
   std::shared_ptr<ImuProcess> p_imu_ = nullptr;                 // imu process
 
-  /// local map related
-  float det_range_ = 300.0f;
-  double cube_len_ = 0;
-
-  float filter_size_surf_min_ = 0.0;
-  float filter_size_map_min_ = 0.0;
-
-  /// params
-  std::vector<double> extrinT_{3, 0.0};  // lidar-imu translation
-  std::vector<double> extrinR_{9, 0.0};  // lidar-imu rotation
-  std::string map_file_path_;
-
   /// point clouds data
   CloudPtr scan_undistort_{new PointCloudType()};   // scan after undistortion
   CloudPtr scan_down_body_{new PointCloudType()};   // downsampled scan in body
@@ -177,18 +161,6 @@ private:
   std::vector<float> residuals_;                    // point-to-plane residuals
   std::vector<bool> point_selected_surf_;           // selected points
   VVec4F plane_coef_;                         // plane coeffs
-
-  /// ros pub and sub stuffs
-//  ros::Subscriber sub_pcl_;
-//  ros::Subscriber sub_imu_;
-//  ros::Publisher pub_laser_cloud_world_;
-//  ros::Publisher pub_laser_cloud_body_;
-//  ros::Publisher pub_laser_cloud_effect_world_;
-//  ros::Publisher pub_match_cloud_world_;
-//  ros::Publisher pub_odom_aft_mapped_;
-//  ros::Publisher pub_path_;
-//  std::string tf_imu_frame_;
-//  std::string tf_world_frame_;
 
   ros::Publisher pub_odom_;
   ros::Publisher pub_path_;
@@ -202,10 +174,8 @@ private:
   std::deque<double> time_buffer_;
   std::deque<PointCloudType::Ptr> lidar_buffer_;
   std::deque<sensor_msgs::Imu::ConstPtr> imu_buffer_;
-  nav_msgs::Odometry odom_aft_mapped_;
 
   /// options
-  bool time_sync_en_ = false;
   double timediff_lidar_wrt_imu_ = 0.0;
   double last_timestamp_lidar_ = 0;
   double lidar_end_time_ = 0;
@@ -218,7 +188,6 @@ private:
   int publish_count_ = 0;
   bool flg_first_scan_ = true;
   bool flg_EKF_inited_ = false;
-  int pcd_index_ = 0;
   double lidar_mean_scantime_ = 0.0;
   int scan_num_ = 0;
   bool timediff_set_flg_ = false;
@@ -229,25 +198,7 @@ private:
   esekfom::esekf<state_ikfom, 12, input_ikfom> kf_;  // esekf
   state_ikfom state_point_;                          // ekf current state
   vect3 pos_lidar_;                                  // lidar position after eskf update
-  Vec3d euler_cur_ = Vec3d::Zero();      // rotation in euler angles
   bool extrinsic_est_en_ = true;
-
-  /////////////////////////  debug show / save /////////////////////////////////////////////////////////
-  bool run_in_offline_ = false;
-  bool path_pub_en_ = true;
-  bool scan_pub_en_ = false;
-  bool dense_pub_en_ = false;
-  bool scan_body_pub_en_ = false;
-  bool scan_effect_pub_en_ = false;
-  bool pcd_save_en_ = false;
-  bool runtime_pos_log_ = true;
-  int pcd_save_interval_ = -1;
-  bool traj_save_en_ = false;
-  std::string dataset_;
-
-//  PointCloudType::Ptr pcl_wait_save_{new PointCloudType()};  // debug save
-//  nav_msgs::Path path_;
-//  geometry_msgs::PoseStamped msg_body_pose_;
 
 };
 
